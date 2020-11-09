f11eaf150f28a67b709b3309a92512cd1cca58ba
Andreas Araya Osorio <66529441+4sito@users.noreply.github.com>
2020-11-09 10:32:24 +0100

    Update gauss.c

--- gauss.c
+++ gauss.c
@@ -2,18 +2,20 @@
 
 int gauss(int n){
 
-int sum = (n * (n + 1))/2;
+  /* formula that calculates the sum with Gauss' method */
+  int gauss_sum = (n * (n + 1))/2;
 
-return sum;
+return gauss_sum;
 }
 
 int main(){
-int n;
 
+  int n;
 
-printf("FORMULA DI GAUSS = SOMMA DI TUTTI I NUMERI N PRECEDENTI.\n n:");
+  /* interaction with user */
+printf("GAUSS = sum of all numbers before a given n\n(n must be a positive integer).\nn:");
 scanf("%d", &n);
-printf("La somma di tutti i numeri naturali precedenti a %d e' %d", n, gauss(n));
+printf("\nGauss' Sum:%d  n:%d", gauss(n), n);
 
 return 0;
 }
